16c7c58a9dc18eb17846a3a86ea6be9371667d74
Yemi <ibrahimsharafadeen95@gmail.com>
2022-06-14 14:19:59 +0100

    done with html and css

---
+++ script.js
@@ -0,0 +1,16 @@
+// const accClick = document.querySelectorAll(".acc-click")
+// accClick.forEach(ach=>{
+//     ach.addEventListener("click",show, false)
+// })
+
+// function show(){
+//     const showit = this.nextElementSibling;
+//     const isHide = showit.classList.contains("hide")
+//     accClick.forEach(ach=>{
+//         const showitem = ach.nextElementSibling
+//         if(!showitem.classList.contains("hide")){
+//             !showitem.classList.add("hide")
+//         }
+
+//     })
+// }
