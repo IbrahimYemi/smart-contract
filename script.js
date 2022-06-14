a283bb93ba4702cf687fd129f2e4b469815ebcb3
Yemi <ibrahimsharafadeen95@gmail.com>
2022-06-14 17:51:02 +0100

    add the toggle bar

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const accClick = document.querySelectorAll(".acc-click")
 accClick.forEach(ach=>{
-    ach.addEventListener("click",show, false)
+    ach.addEventListener("click",showacc, false)
 })
 
-function show(){
+function showacc(){
     const showit = this.nextElementSibling;
     const isHide = showit.classList.contains("hide")
     accClick.forEach(ach=>{
@@ -15,4 +15,14 @@ function show(){
     if(isHide){
         showit.classList.remove("hide")
     }
+}
+
+function showtabs(){
+    const navtag = document.querySelector("nav");
+    const tabs = document.querySelector(".fa-bars");
+
+    tabs.addEventListener("click",()=>{
+        tabs.classList.toggle("fa-times")
+        navtag.classList.toggle("tabs")
+    })
 }
