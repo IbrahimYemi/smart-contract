b8e20c2bb334d621b6cbd78b5114da054d604843
Yemi <ibrahimsharafadeen95@gmail.com>
2022-06-14 15:05:19 +0100

    work completed, add accordion

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-// const accClick = document.querySelectorAll(".acc-click")
-// accClick.forEach(ach=>{
-//     ach.addEventListener("click",show, false)
-// })
+const accClick = document.querySelectorAll(".acc-click")
+accClick.forEach(ach=>{
+    ach.addEventListener("click",show, false)
+})
 
-// function show(){
-//     const showit = this.nextElementSibling;
-//     const isHide = showit.classList.contains("hide")
-//     accClick.forEach(ach=>{
-//         const showitem = ach.nextElementSibling
-//         if(!showitem.classList.contains("hide")){
-//             !showitem.classList.add("hide")
-//         }
-
-//     })
-// }
+function show(){
+    const showit = this.nextElementSibling;
+    const isHide = showit.classList.contains("hide")
+    accClick.forEach(ach=>{
+        const showitem = ach.nextElementSibling
+        if(!showitem.classList.contains("hide")){
+            showitem.classList.add("hide")
+        }
+    });
+    if(isHide){
+        showit.classList.remove("hide")
+    }
+}
